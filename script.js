a8f63b1a49b33fdf7b7da8081ad7132579a0122e
Alexandre B Radtke <alexandre.bluehouse@gmail.com>
2020-05-16 11:45:20 -0400

    iniciando a lista de convidados

---
+++ script.js
@@ -0,0 +1,26 @@
+let convidados = []
+
+function adicionar() {
+    // 1) pegar os valores
+    let nome = document.getElementById("nome").value // adiciona um novo nome lista "convidados"
+    let idade = document.getElementById("idade").value
+    let email = document.getElementById("email").value
+    // 2) adicionar o vetor
+    convidados.push({ nome, idade, email })
+    // 3) atualizar o HTML
+    atualizarLista()
+}
+
+function atualizarLista() {
+    //Pegar a section #Lista
+    let lista = document.getElementById("lista")
+    //limpar a lista
+    lista.innerHTML = ""
+    // adiciona os elementos
+    for (i = 0; i < convidados.length; i++) {
+        lista.innerHTML += `
+        <div>
+            <span>${convidados[i].nome}</span>
+        </div>`
+    }
+}
